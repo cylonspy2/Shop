26e29d6455da41a0b08009f15b52b504b8688f52
Rowan Brown <rowanbrown644@gmail.com>
2020-02-19 16:02:39 -0500

    Added UI stuff and a score counter

---
+++ Assets/Resources/Scripts/scoreCount.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
+
+public class scoreCount : MonoBehaviour
+{
+
+    public GameObject score1;
+    public GameObject score2;
+    public GameObject score3;
+    public Text score;
+    public int startingScore = 0;
+    private int currentScore;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentScore = startingScore;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        score.text = currentScore.ToString();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        currentScore++;
+        
+    }
+}
